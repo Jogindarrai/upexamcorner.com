71de8cbe88ed1f16f6f31ba98993806b51eee716
Jogindar <jogindar.rw@gmail.com>
2025-08-29 17:38:07 +0530

    WP site update

--- wp-content/themes/mywp/functions.php
+++ wp-content/themes/mywp/functions.php
@@ -6,12 +6,11 @@ function my_custom_theme_setup() {
 }
 add_action('after_setup_theme', 'my_custom_theme_setup');
 
+
 function my_custom_theme_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'my_custom_theme_scripts');
-
-
 
 
 function register_my_menus() {
